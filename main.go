89636c0be6d134afddee0d5ab90dff98a06f11cc
Kare Nuorteva <kare.nuorteva@me.com>
2017-04-24 12:28:16 +0300

    Initial version

---
+++ main.go
@@ -0,0 +1,73 @@
+package main // import "kkn.fi/cmd/tcpproxy"
+
+// http://pub.gajendra.net/src/trampoline.c
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"path"
+	"sync"
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: %v sourceport host port\n", path.Base(os.Args[0]))
+}
+
+func main() {
+	log.SetFlags(0)
+	log.SetPrefix("tcpproxy: ")
+	flag.Usage = usage
+	if len(os.Args) != 4 {
+		usage()
+		os.Exit(2)
+	}
+	laddr := net.JoinHostPort("", os.Args[1])
+	raddr := net.JoinHostPort(os.Args[2], os.Args[3])
+	serve(laddr, raddr)
+}
+
+func serve(laddr, raddr string) {
+	proxy, err := net.Listen("tcp", laddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		local, err := proxy.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go func(local net.Conn) {
+			remote, err := net.Dial("tcp", raddr)
+			if err != nil {
+				log.Print(err)
+				if err := local.Close(); err != nil {
+					log.Println(err)
+				}
+				return
+			}
+			var wg sync.WaitGroup
+			wg.Add(2)
+			go xfer(local, remote, &wg)
+			go xfer(remote, local, &wg)
+			wg.Wait()
+			if err := remote.Close(); err != nil {
+				log.Println(err)
+			}
+			if err := local.Close(); err != nil {
+				log.Println(err)
+			}
+		}(local)
+	}
+}
+
+func xfer(dst io.Writer, src io.Reader, wg *sync.WaitGroup) {
+	defer wg.Done()
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Print(err)
+	}
+}
